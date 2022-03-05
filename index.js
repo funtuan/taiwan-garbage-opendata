40c916c5d5fddc5b61631f2cb4c83a3eed2022d0
funtuan <hank.chen@ohbottech.com>
2022-03-06 00:21:06 +0800

    Add open-data push github

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const Cron = require('croner');
+const simpleGit = require('simple-git')
+const process = require('./process')
+
+async function run() {
+  console.log('running...')
+
+  await simpleGit()
+    .checkout('master')
+    .branch(['-D', 'open-data'])
+    .checkoutLocalBranch('open-data')
+
+  await process()
+
+  await simpleGit()
+    .add('./data/*')
+    .commit(`open-data: ${Date.now()}`)
+    .push(['-f', 'origin', 'open-data'])
+    .catch((err) => console.error('failed: ', err));
+
+  console.log('done')
+}
+
+Cron('*/10 * * * * *', run)
---
+++ process.js
@@ -0,0 +1,10 @@
+
+
+const fs = require('fs')
+
+module.exports = async () => {
+  await fs.writeFileSync('./data/now.json', JSON.stringify({
+    msg: 'hello world',
+    timestamp: Date.now()
+  }))
+}
